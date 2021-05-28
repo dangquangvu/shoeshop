776c935cda928a4fcfd36bce03825074cea424d1
vu dang <vu.dang@sotatek.com>
2021-05-29 00:29:44 +0700

    create product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
+import { ProductModule } from './modules/product/product.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
 // import { CommentModule } from './modules/comment/comment.module';
 
@@ -14,7 +15,8 @@ import { TransactionModule } from './modules/transaction/transaction.module';
       useCreateIndex: true,
     }),
     AuthModule,
-    TransactionModule
+    TransactionModule,
+    ProductModule
   ],
 })
 export class AppModule { }
